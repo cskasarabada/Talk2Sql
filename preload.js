c42f76e3ee69d68617f635331de7572f56622d21
cskasarabada <cskasarabada@gmail.com>
2026-05-29 23:50:44 -0500

    Add toggleMaximize function to electronAPI

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   saveFile: (opts) => ipcRenderer.invoke('save-file', opts),
+  toggleMaximize: () => ipcRenderer.invoke('toggle-maximize'),
   onMenuEvent: (cb) => {
     ['menu-run','menu-save','menu-new-tab','menu-format','menu-explain',
      'menu-connect','menu-export-excel','menu-export-csv'].forEach(e => {
